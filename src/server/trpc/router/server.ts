596e358fb711a450d2fa46baebeb880ad454cfa8
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2022-12-13 00:32:19 -0300

    ✨ Create channels and send messages

--- src/server/trpc/router/server.ts
+++ src/server/trpc/router/server.ts
@@ -25,4 +25,66 @@ export const serverRouter = router({
         },
       });
     }),
+  show: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.server.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          channels: true,
+        },
+      });
+    }),
+  createChannel: publicProcedure
+    .input(z.object({ name: z.string(), serverId: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.channel.create({
+        data: {
+          name: input.name,
+          server: {
+            connect: {
+              id: input.serverId,
+            },
+          },
+        },
+      });
+    }),
+  getChannel: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.channel.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          messages: true,
+        },
+      });
+    }),
+  createMessage: publicProcedure
+    .input(
+      z.object({
+        content: z.string(),
+        channelId: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.message.create({
+        data: {
+          content: input.content,
+          channel: {
+            connect: {
+              id: input.channelId,
+            },
+          },
+          author: {
+            connect: {
+              id: ctx.session?.user?.id,
+            },
+          },
+        },
+      });
+    }),
 });
